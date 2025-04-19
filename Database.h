4c8a91cde34265d70fa879fe79a6bb7a75126aa9
taraj <tarajimin57@gmail.com>
2025-04-19 21:30:14 +0330

    .

--- Database.h
+++ Database.h
@@ -1,5 +1,7 @@
-#include<iostream>
 using namespace std;
 database{
+public:
+    void insertRecord{
 
+    }
 };
--- Scripts/main.cpp
+++ Scripts/main.cpp
@@ -4,6 +4,5 @@
 #include<iostream>
 using namespace std;
 int main (){
-    cout << "Hello World" ;
-    return 0;
+
 }
