23bf58d29b098c00d7c94b4cf0122b551a520877
taraj <tarajimin57@gmail.com>
2025-04-25 22:53:50 +0330

    tara 1`s last commit

--- Schema.h
+++ Schema.h
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 class Schema {
 public:
-    vector<string> Column;
-    Schema (const vector<string>& column) : Column(column) {}
+    string Tablename;
+    vector<Column> Columns;
+    Schema (const string& tablename, const vector<Column>& columns) {
+        Tablename = tablename;
+        Columns = columns;
+    }
+    void displayColumn() const {
+        cout << "Column In Table: " << Tablename << ":" << endl;
+        for (auto it = Columns.begin(); it != Columns.end(); ++it) {
+            const auto& column = *it;
+            cout << Columns.TableName << " (" << Columns.DataType << ")" << endl;
+        }
+    }
+    bool ColumnExist(const string& columnTableName) const {
+        for (auto it = Columns.begin(); it != Columns.end(); ++it) {
+            const auto& columns = *it;
+            if (Columns.TableName = columnTableName){
+                return true;}}
+        return false;
+    }
+    void addColumn(const string& TableName, const string& DataType){
+        Columns.emplace_back(TableName, DataType);
+    }
 };
